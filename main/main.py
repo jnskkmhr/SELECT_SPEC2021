b2609d6452fee33e46c1b35cb447a3bc9141798c
jnskkmhr <junjun.k2012@gmail.com>
2021-11-23 17:37:11 +0900

    fixed main.py

--- main/main.py
+++ main/main.py
@@ -6,7 +6,10 @@ import sys
 import time 
 
 def main(): 
-    distance = 100 
-    spec = {"radius": 0.3, "height": 2}
+    distance = 100 # in meter  
+    spec = {"radius": 0.3, "height": 2} # in meter
     res = Resilience(distance, spec)
     res.run()
+
+if __name__ == "__main__": 
+    main()
