0970fd87a196ef7b30f464da3be248eeee783312
Ryo-Kuzuno <kuzuno1027@gmail.com>
2022-04-30 20:12:44 +0900

    Update e2s_test5

--- src/test/e2s_test.py
+++ src/test/e2s_test.py
@@ -14,7 +14,10 @@ pin_e2s_top = 16
 pin_e2s_bottom = 20
 
 e2s_ = E2S(pin_e2s_top, pin_e2s_bottom)
-print("pin_e2s_top = %.1f\n pin_e2s_bottom = %.1f\n", pin_e2s_top, pin_e2s_bottom)
+gpio.setup(pin_e2s_top, gpio.IN, pull_up_down=gpio.PUD_DOWN)
+gpio.setup(pin_e2s_bottom, gpio.IN, pull_up_down=gpio.PUD_DOWN)
+
+print("pin_e2s_top = %d\n" %pin_e2s_top)
 
 while True:
   try:
