814ad1bd1508008aa31ddbb2a03429eefdb5bee9
Ryo-Kuzuno <kuzuno1027@gmail.com>
2022-04-30 18:10:02 +0900

    Update e2s_test1

--- src/test/e2s_test.py
+++ src/test/e2s_test.py
@@ -1,6 +1,7 @@
 import sys
 sys.path.append('../')
 from selemod import E2S
+from time import sleep, time
 import time
 import RPi.GPIO
 
@@ -13,12 +14,13 @@ pin_e2s_top = 16
 pin_e2s_bottom = 20
 
 e2s_ = E2S(pin_e2s_top, pin_e2s_bottom)
-print("instantiation cuccessed!")
+print("instantiation succeeded!")
 
 while True:
   try:
       top_sw_state = e2s_.read_top()
       print(top_sw_state)
+      sleep(1)
   except KeyboardInterrupt:
      print("Operation was killed!")
      e2s_.destroy()
