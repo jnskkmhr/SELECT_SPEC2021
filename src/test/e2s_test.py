bd7fc6c42ff32df1510d4b0c983e574a6262d310
Ryo-Kuzuno <kuzuno1027@gmail.com>
2022-05-13 08:58:17 +0900

    E2S_switch test succeeded!

--- src/test/e2s_test.py
+++ src/test/e2s_test.py
@@ -8,24 +8,30 @@ import RPi.GPIO as gpio
 #このプログラムの起動は時限式にする
 # もしくは、クライマーの高度が上端のダンパー付近に来た時に、起動する
 
-# GPIO.setmode(GPIO.BCM)
+gpio.setmode(gpio.BCM)
 
 pin_e2s_top = 16
 pin_e2s_bottom = 20
 
 e2s_ = E2S(pin_e2s_top, pin_e2s_bottom)
-gpio.setup(pin_e2s_top, gpio.IN, pull_up_down=gpio.PUD_DOWN)
+
+gpio.setup(pin_e2s_top,    gpio.OUT, initial=gpio.LOW)
+gpio.setup(pin_e2s_bottom, gpio.OUT, initial=gpio.LOW)
+gpio.setup(pin_e2s_top,    gpio.IN, pull_up_down=gpio.PUD_DOWN)
 gpio.setup(pin_e2s_bottom, gpio.IN, pull_up_down=gpio.PUD_DOWN)
 
 print("pin_e2s_top = %d\n" %pin_e2s_top)
+print("pin_e2s_bottom = %d\n" %pin_e2s_bottom)
 
 while True:
   try:
-      top_sw_state = e2s_.read_top()
-      print(top_sw_state)
+      top_sw_state    = e2s_.read_top()
+      bottom_sw_state = e2s_.read_bottom()
+      print("top:",top_sw_state, "bottom",bottom_sw_state)
       sleep(1)
   except KeyboardInterrupt:
      print("Operation was killed!")
      e2s_.destroy()
+     gpio.cleanup()
      break
 
