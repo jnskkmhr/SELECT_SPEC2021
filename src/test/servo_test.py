fd969fa16124b185f65503e423ba04592290b193
Ryo-Kuzuno <kuzuno1027@gmail.com>
2022-05-13 12:49:57 +0900

    Modify servo_test

--- src/test/servo_test.py
+++ src/test/servo_test.py
@@ -6,14 +6,13 @@ import RPi.GPIO as gpio
 
 pin_esc = 12
 pin_servo_1 = 18
-pin_servo_2 = 24 
 freq_esc = 50 
 freq_servo = 50 
 # servo duty min. = 4.85%, max. = 9.85%
 brakeon_duty = 8.72 #4.85 + (9.85-4.85)*60/90
 brakeoff_duty = 4.85
 
-actu = selemod.Actuator(pin_esc=pin_esc, pin_servo_1=pin_servo_1, pin_servo_2=pin_servo_2, freq_esc=freq_esc, freq_servo=freq_servo, brakeon_duty=brakeon_duty, brakeoff_duty=brakeoff_duty)
+actu = selemod.Actuator(pin_esc=pin_esc, pin_servo_1=pin_servo_1, freq_esc=freq_esc, freq_servo=freq_servo, brakeon_duty=brakeon_duty, brakeoff_duty=brakeoff_duty)
 
 while True: 
     try: 
